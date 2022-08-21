3a95e240e07fb00bea2a1aac93d91aaa92ee91c0
Soytaku <111513649+Soytaku@users.noreply.github.com>
2022-08-21 11:06:10 -0300

    Add files via upload

---
+++ plugins/creditos.js
@@ -0,0 +1,18 @@
+
+let handler = async m => m.reply(`
+╭─════「 *𝙲𝚁𝙴𝙳𝙸𝚃𝙾𝚂* 」═════
+│─≽ 𝙱𝙾𝚃 𝙲𝚁𝙴𝙰𝙳𝙾 𝙿𝙾𝚁: 𝚃𝙰𝙺𝚄
+│─≽ 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙾: wa.me/5493772642151
+│─≽ 𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃: 
+│─≽ https://github.com/
+╰────────────────
+*𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴𝙻 𝙱𝙾𝚃*, 
+
+「𝙶𝚁𝚄𝙿𝙾 𝟷」 ~https://chat.whatsapp.com/BVYbhiEXHzT2gTJXmfZC98~
+
+「𝙶𝚁𝚄𝙿𝙾 𝟸」 ~https://chat.whatsapp.com/HmHiPZqDyCZKuCJC2nuUkq~
+`.trim()) 
+handler.help = ['creditos']
+handler.tags = ['main']
+handler.command = /^(creditos|créditos|creadores)$/i
+export default handler
