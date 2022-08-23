dc5f512c495e6c3a32c53ab8d3fb20519c765435
Soytaku <111513649+Soytaku@users.noreply.github.com>
2022-08-22 23:46:25 -0300

    Add files via upload

---
+++ plugins/juegos-porcentajes.js
@@ -0,0 +1,41 @@
+// by odin el fucker
+
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+    if (!text) throw `*[❗] 𝙿𝙰𝚁𝙰 𝙲𝙰𝙻𝙲𝚄𝙻𝙰𝚁 𝙴𝙻 𝙿𝙾𝚁𝙲𝙴𝙽𝚃𝙰𝙶𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙸𝙴𝙽 𝚄𝚂𝙰 𝙲𝙾𝙼𝙾 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:* ${usedPrefix}${command} @usuario`
+    
+let set // crear variable de base da tos
+
+if (/gay/.test(command)) set = '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙶𝙰𝚈,🏳️‍🌈 𝙻𝙴 𝙴𝚂𝙲𝙰𝙽𝚃𝙰 𝙴𝙻 𝙽𝙴𝙿𝙴🤭*'
+if (/pendejo/.test(command)) set = '*😂𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙿𝙴𝙽𝙳𝙴𝙹𝙾, 𝚂𝙴 𝙻𝙴 𝙲𝙰𝚈𝙾 𝙴𝙻 𝙲𝙴𝚁𝙴𝙱𝚁𝙾🧠 𝙿𝙾𝚁 𝙼𝙾𝙽𝙶𝙾🤓*'
+if (/maricon/.test(command)) set = '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙼𝙰𝚁𝙸𝙲𝙾𝙽,🤭 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙰𝙼𝙴 𝙿𝙴𝚁𝙾 𝚂𝙴 𝚃𝙴 𝙲𝙰𝚈𝙾👗 𝙻𝙰 𝙵𝙰𝙻𝙳𝙰 𝙹𝙰𝙹𝙰𝙹𝙰😹*'
+if (/chad/.test(command)) set = '*𝙲𝙷𝙰𝙳 😎 𝚂𝙸 𝙻𝙰𝚂 𝙿𝙾𝙽𝙴 𝙳𝙴 𝚁𝙾𝙳𝙸𝙻𝙻𝙰𝚂 𝙽𝙾 𝚁𝙴𝚉𝙴𝙽 𝙿𝙾𝚁 𝙴𝙻* 🤫'
+if (/fucker/.test(command)) set = '*𝙵𝚄𝙲𝙺𝙴𝚁😎,  𝚀𝚄𝙴 𝙽𝙾 𝚃𝙴 𝙳𝙴 𝚂𝙸𝙳𝙰 𝙲𝚁𝙰𝙲𝙺* 🥵'
+if (/sad/.test(command)) set = '*𝙼𝙾𝙳𝙾 𝚂𝙰𝙳😞, 𝙼𝙰𝙽𝙳𝙴𝙽 𝙿𝙾𝚁𝙽𝙾 𝙿𝙰𝚁𝙰 𝙰𝙽𝙸𝙼𝙰𝚁𝙻𝙾😈*'
+if (/kunno/.test(command)) set = '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙵𝙰𝙽 𝙳𝙴 𝙺𝚄𝙽𝙽𝙾🤢, 𝚂𝙰𝚀𝚄𝙴𝙽𝙻𝙾 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾🚮 𝚈 𝚅𝙸𝙾𝙻𝙴𝙽𝙻𝙾 𝙳𝙴 𝙱𝙸𝙽𝙰𝚁𝙸𝙾𝚂😈*'  //<<<<<<<<<< crear if con el comando y la variable anterior e informacion
+if (/bts/.test(command)) set = '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴/𝙰 𝙵𝙰𝙽 𝙳𝙴 𝙱𝚃𝚂🤪 𝙰𝚁𝙼𝚈 𝙳𝙴𝚃𝙴𝙲𝚃𝙰𝙳𝙰📣*'
+if (/badass/.test(command)) set = '*𝙱𝙰𝙳𝙰𝚂𝚂, 😎𝙱𝙸𝙴𝙽 𝙾𝙿 𝙴𝙻 𝙿𝙰𝙽𝙰💪🏻*'
+if (/arrogante/.test(command)) set = '*𝙰𝚁𝚁𝙾𝙶𝙰𝙽𝚃𝙴😡, 𝚃𝙰𝙻𝚅𝙴𝚉 𝚂𝙸𝙲𝙰𝚁𝙸𝙾𝚂 𝙻𝙾 𝙰𝙶𝙰𝙽 𝚁𝙴𝙵𝙻𝙴𝚇𝙸𝙾𝙽𝙰𝚁.𝚓𝚙𝚐🫥*'
+if (/verguenza/.test(command)) set = '*𝚂𝙸𝙽 𝚅𝙴𝚁𝙶𝚄𝙴𝙽𝚉𝙰,😶 𝙿𝙰𝚁𝙴𝙲𝙴 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙴 𝚀𝚄𝙴 𝙻𝙾 𝙳𝙾𝚇𝙴𝙴𝙽😈*'
+if (/cristiano/.test(command)) set = '*𝙲𝚁𝙸𝚂𝚃𝙸𝙰𝙽𝙾😇*'
+if (/basado/.test(command)) set = '*𝙱𝙰𝚂𝙰𝙳𝙾, 𝚃𝙾𝙳𝙰 𝙻𝙰 𝚁𝙰𝚉𝙾𝙽😎*'
+if (/puto/.test(command)) set = '*😂𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙿𝚄𝚃𝙾🌈*'
+if (/puta/.test(command)) set = '𝙼𝙸𝚁𝙴𝙽 𝙷𝙰𝚈 𝚄𝙽𝙰 𝙿𝚄𝚃𝙰, 𝙼𝙴 𝙻𝙰 𝙼𝙰𝙼𝙰𝚂?🥵'
+
+    conn.reply(m.chat, `
+  ${command} *${text}*
+  *${text}* es *${(1000).getRandom()}*% ${set.replace('es', set )}
+  `.trim(), m, m.mentionedJid ? {
+        mentions: m.mentionedJid
+    } : {})
+
+
+}
+handler.help = ['pendejo', 'gay', 'chad', 'fucker', 'sad', 'kunno', 'bts', 'badass', 'arrogante', 'verguenza', 'cristiano',
+'basado', 'puto','puta']
+handler.tags = ['juegos']
+handler.command = /^(gay|pendejo|maricon|chad|fucker|sad|kunno|bts|badass|arrogante|verguenza|cristiano|basado|puta|puto)/i
+handler.group = true
+export default handler
+
+// para agregar mas solo copiar una linea if, colocar comando junto con '' y agregar al handler.command
